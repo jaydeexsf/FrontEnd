904d9f6d7e6c2cb8113bb8b73820043272a89461
johannes <mojohannes06@gmail.com>
2025-04-03 12:36:12 +0200

    03 April 2025 #4

--- src/assets/resources/ContactData.jsx
+++ src/assets/resources/ContactData.jsx
@@ -1,6 +1,6 @@
 const ContactData = {
       "name": " Johannes Moloantoa",
-      "age": 21,
+      "age": " " + 21,
       "email": " jaydeexsf@gmail.com",
       "phone": " +27 79 492 0938",
       "country": " South Africa",
